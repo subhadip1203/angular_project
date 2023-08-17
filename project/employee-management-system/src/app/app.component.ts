6f550fc2eca7f7bec407075cdb25fd3c03f6e06e
subhadip1203 <subhadip1203@gmail.com>
2023-08-17 16:31:00 -0700

    new project employee management

---
+++ project/employee-management-system/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'employee-management-system';
+
+  constructor(private _dialog:MatDialog) { }
+
+  openAddEditDialog(){
+    this._dialog.open(EmpAddEditComponent)
+  }
+
+}
