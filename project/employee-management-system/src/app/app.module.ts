014f4497e11daaec2f9d15e060902a3a76c53b21
subhadip1203 <subhadip1203@gmail.com>
2023-08-21 11:42:56 -0700

    form basic

--- project/employee-management-system/src/app/app.module.ts
+++ project/employee-management-system/src/app/app.module.ts
@@ -16,6 +16,7 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatNativeDateModule} from '@angular/material/core';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatSelectModule} from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import {MatSelectModule} from '@angular/material/select';
     MatDatepickerModule,
     MatNativeDateModule,
     MatRadioModule,
-    MatSelectModule
+    MatSelectModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- project/employee-management-system/src/app/emp-add-edit/emp-add-edit.component.ts
+++ project/employee-management-system/src/app/emp-add-edit/emp-add-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-emp-add-edit',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./emp-add-edit.component.scss']
 })
 export class EmpAddEditComponent {
+
+  empForm : FormGroup
+
+  constructor(private _formBuilder: FormBuilder) {
+    this.empForm = this._formBuilder.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      country: '',
+      dob:'',
+      gender: '',
+      education:'',
+      company: '',
+    })
+  }
+
+  onFromSubmit(){
+    console.log(this.empForm.value);
+  }
+
 
 }
